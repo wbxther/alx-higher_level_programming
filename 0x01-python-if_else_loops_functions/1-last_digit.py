c6da7516397f19e3cb265adef8e41d915e920dac
wbxther <wunmiiyaomolere@gmail.com>
2022-09-22 04:44:23 +0000

    python_loop

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ print("Last digit of {} is {} and is ".format(number, remainder), end="")
 if remainder > 5:
     print("greater than 5")
 elif remainder == 0:
-    print("0")
+    print("and is 0")
 else:
     print("less than 6 and not 0")
