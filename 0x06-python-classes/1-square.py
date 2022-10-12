fa7abf46a101d476759c6f7c0f572d7645c43fe4
wbxther <wunmiiyaomolere@gmail.com>
2022-10-12 03:48:14 +0000

    class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin!/python3
 ### creating 1-square a subset of 0-square###
 class Square(Square):
-   def __init__(self, size):
-      self.__size = size
+    def __init__(self, size=0):
+       self.size = size
+
+    @property
+    def size(self):
+       return self.__size
